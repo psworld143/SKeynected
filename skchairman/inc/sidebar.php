d2918e4aa26e14483f0d76c4f9ace8669615125d
Kristian Jay Paculanan <paculanankj@gmail.com>
2024-10-02 11:03:00 +0800

    budget-request

--- skchairman/inc/sidebar.php
+++ skchairman/inc/sidebar.php
@@ -1,36 +1,43 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+
 <aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
 
-  <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link <?php echo ($current_page == 'index.view.php') ? 'active' : ''; ?>"
+                href="index.view.php">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <hr>
 
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="#">
-        <i class="bi bi-grid"></i>
-        <span>Dashboard</span>
-      </a>
-    </li>
-    <hr>
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="#">
-        <i class="bi bi-person"></i>
-        <span>Youth</span>
-      </a>
-    </li>
-    <hr>
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="#">
-        <i class="bi bi-briefcase"></i>
-        <span>Projects</span>
-      </a>
-    </li>
-    <hr>
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="#">
-        <i class="bi bi-cash-stack"></i>
-        <span>Budget Request</span>
-      </a>
-    </li>
+        <li class="nav-item">
+            <a class="nav-link <?php echo ($current_page == '#') ? 'active' : ''; ?>" href="#">
+                <i class="bi bi-person"></i>
+                <span>Youth</span>
+            </a>
+        </li>
+        <hr>
 
+        <li class="nav-item">
+            <a class="nav-link <?php echo ($current_page == '#') ? 'active' : ''; ?>" href="#">
+                <i class="bi bi-briefcase"></i>
+                <span>Projects</span>
+            </a>
+        </li>
+        <hr>
 
-  </ul>
+        <li class="nav-item">
+            <a class="nav-link <?php echo ($current_page == 'budget-request.php') ? 'active' : ''; ?>"
+                href="budget-request.php">
+                <i class="bi bi-cash-stack"></i>
+                <span>Budget Request</span>
+            </a>
+        </li>
+        <hr>
 
+    </ul>
 </aside><!-- End Sidebar-->
