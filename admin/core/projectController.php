bbeefed2677f50089578c8ced5de57b74f3f0f76
St4ckkk <keyanandydelgado@gmail.com>
2024-10-08 10:48:36 +0800

    can set budget hearing schedule

--- admin/core/projectController.php
+++ admin/core/projectController.php
@@ -13,14 +13,16 @@ class projectController
     }
 
 
-    public function getProjects() {
+    public function getProjects()
+    {
         $query = "SELECT * FROM projects";
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll();
     }
 
-    public function getProjectsByID($project_id) {
+    public function getProjectsByID($project_id)
+    {
         $query = "SELECT * FROM projects WHERE project_id = :project_id";
         $stmt = $this->db->prepare($query);
         $params = [':project_id' => $project_id];
@@ -28,11 +30,27 @@ class projectController
         return $stmt->fetch();
     }
 
-    public function getMaterialsByProjectID($project_id) {
+    public function getMaterialsByProjectID($project_id)
+    {
         $query = "SELECT * FROM materials WHERE project_id = :project_id";
         $stmt = $this->db->prepare($query);
         $params = [':project_id' => $project_id];
         $stmt->execute($params);
         return $stmt->fetchAll();
+    }
+
+    public function updateStatus($project_id, $status, $hearing_date = null)
+    {
+
+        $query = "UPDATE projects SET status = :status, hearing_schedule = :hearing_schedule WHERE project_id = :project_id";
+        $stmt = $this->db->prepare($query);
+
+        $params = [
+            ':project_id' => $project_id,
+            ':status' => $status,
+            ':hearing_schedule' => $hearing_date
+        ];
+
+        return $stmt->execute($params);
     }
 }
